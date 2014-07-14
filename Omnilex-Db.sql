d59eb0501f0249281ae4f6365632df4353f7c3c5
Khanh Nguyen <knultimate@live.com>
2014-07-14 08:43:25 -0700

    Added Latin Alphabet
    This alphabet is the root of all languages. Cyrillic, Hangul, Hiragana/Katakana/Kanji, and Hanzi are another sets of alphabets for Russian, Korean, Japanese, and Chinese.
    TODO: Find a way to link this with extended alphabets, those that are only available to some languages (maybe in the logic and not database?)

--- Omnilex-Db.sql
+++ Omnilex-Db.sql
@@ -28,7 +28,7 @@ CREATE TABLE Languages (
   Lang_ID   int IDENTITY(1,1),
   Lang      varchar(50) NOT NULL,
   CONSTRAINT [PK_LangID] PRIMARY KEY (Lang_ID ASC),
-  CONSTRAINT [CHK_Lang] CHECK (DATALENGTH(Lang))
+  CONSTRAINT [CHK_Lang] CHECK (DATALENGTH(Lang) > 0)
 );
 
 INSERT INTO Languages (Lang) VALUES ('English');
@@ -38,7 +38,7 @@ INSERT INTO Languages (Lang) VALUES ('Russian');
 INSERT INTO Languages (Lang) VALUES ('Vietnamese');
 
 /********************************************************************************
-                            ALPHABETS RELATION
+                            LATIN ALPHABET RELATION
 *********************************************************************************/
 IF EXISTS (SELECT * FROM sys.default_constraints WHERE name = N'PK_AlphaID') 
   ALTER TABLE LatinAlphabet DROP CONSTRAINT [PK_AlphaID];
@@ -48,15 +48,32 @@ CREATE TABLE LatinAlphabet (
   Alpha_ID   int IDENTITY(1,1),
   Character  varchar(5) NOT NULL,
   CONSTRAINT [PK_AlphaID] PRIMARY KEY (Alpha_ID ASC),
-  CONSTRAINT [CHK_Char] CHECK (DATALENGTH(Character))
+  CONSTRAINT [CHK_Char] CHECK (DATALENGTH(Character) > 0)
 );
 
-INSERT INTO LatinAlphabet (Lang) VALUES ('A');
-INSERT INTO LatinAlphabet (Lang) VALUES ('B');
-INSERT INTO LatinAlphabet (Lang) VALUES ('C');
-INSERT INTO LatinAlphabet (Lang) VALUES ('D');
-INSERT INTO LatinAlphabet (Lang) VALUES ('D');
-INSERT INTO LatinAlphabet (Lang) VALUES ('D');
-INSERT INTO LatinAlphabet (Lang) VALUES ('D');
-INSERT INTO LatinAlphabet (Lang) VALUES ('D');
-INSERT INTO LatinAlphabet (Lang) VALUES ('D');
+INSERT INTO LatinAlphabet (Character) VALUES ('A');
+INSERT INTO LatinAlphabet (Character) VALUES ('B');
+INSERT INTO LatinAlphabet (Character) VALUES ('C');
+INSERT INTO LatinAlphabet (Character) VALUES ('D');
+INSERT INTO LatinAlphabet (Character) VALUES ('E');
+INSERT INTO LatinAlphabet (Character) VALUES ('F');
+INSERT INTO LatinAlphabet (Character) VALUES ('G');
+INSERT INTO LatinAlphabet (Character) VALUES ('H');
+INSERT INTO LatinAlphabet (Character) VALUES ('I');
+INSERT INTO LatinAlphabet (Character) VALUES ('J');
+INSERT INTO LatinAlphabet (Character) VALUES ('K');
+INSERT INTO LatinAlphabet (Character) VALUES ('L');
+INSERT INTO LatinAlphabet (Character) VALUES ('M');
+INSERT INTO LatinAlphabet (Character) VALUES ('N');
+INSERT INTO LatinAlphabet (Character) VALUES ('O');
+INSERT INTO LatinAlphabet (Character) VALUES ('P');
+INSERT INTO LatinAlphabet (Character) VALUES ('Q');
+INSERT INTO LatinAlphabet (Character) VALUES ('R');
+INSERT INTO LatinAlphabet (Character) VALUES ('S');
+INSERT INTO LatinAlphabet (Character) VALUES ('T');
+INSERT INTO LatinAlphabet (Character) VALUES ('U');
+INSERT INTO LatinAlphabet (Character) VALUES ('V');
+INSERT INTO LatinAlphabet (Character) VALUES ('W');
+INSERT INTO LatinAlphabet (Character) VALUES ('X');
+INSERT INTO LatinAlphabet (Character) VALUES ('Y');
+INSERT INTO LatinAlphabet (Character) VALUES ('Z');
