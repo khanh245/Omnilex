9ed99ec390e2fad74d7d29bea7f2f4bca58a11f7
Khanh Nguyen <knultimate@live.com>
2014-07-13 23:28:59 -0700

    Initial work on Alphabet
    Should be divided into some common alphabets or not....

--- Omnilex-Db.sql
+++ Omnilex-Db.sql
@@ -36,3 +36,24 @@ INSERT INTO Languages (Lang) VALUES ('French');
 INSERT INTO Languages (Lang) VALUES ('Italian');
 INSERT INTO Languages (Lang) VALUES ('Russian');
 INSERT INTO Languages (Lang) VALUES ('Vietnamese');
+
+/********************************************************************************
+                            ALPHABETS RELATION
+*********************************************************************************/
+/* TODO: Make up alphabet list for different languages. */
+IF EXISTS (SELECT * FROM sys.default_constraints WHERE name = N'PK_LangID') 
+  ALTER TABLE Languages DROP CONSTRAINT [PK_LangID];
+IF EXISTS (SELECT * FROM information_schema.tables WHERE table_name = N'Languages') 
+  DROP TABLE Languages;
+CREATE TABLE Languages (
+  Lang_ID   int IDENTITY(1,1),
+  Lang      varchar(50) NOT NULL,
+  CONSTRAINT [PK_LangID] PRIMARY KEY (Lang_ID ASC),
+  CONSTRAINT [CHK_Lang] CHECK (DATALENGTH(Lang) > 0 AND DATALENGTH(Lang) > 0)
+);
+
+INSERT INTO Languages (Lang) VALUES ('English');
+INSERT INTO Languages (Lang) VALUES ('French');
+INSERT INTO Languages (Lang) VALUES ('Italian');
+INSERT INTO Languages (Lang) VALUES ('Russian');
+INSERT INTO Languages (Lang) VALUES ('Vietnamese');
