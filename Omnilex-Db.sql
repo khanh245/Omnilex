882ba66991a8871731be3da04840a1b3dce7a8d9
Khanh Nguyen <knultimate@live.com>
2014-07-14 20:16:30 -0700

    Added Language and Alphabet Joint Relation

--- Omnilex-Db.sql
+++ Omnilex-Db.sql
@@ -1,7 +1,6 @@
 /********************************************************************************
         GENERAL PURPOSE SCRIPTS - SETTING UP ENVIRONMENT
 *********************************************************************************/
-
 SET NOCOUNT ON;
 GO
 
@@ -16,26 +15,6 @@ GO
 
 USE [OmniLexDb];
 GO
-
-/********************************************************************************
-                            LANGUAGES RELATION
-*********************************************************************************/
-IF EXISTS (SELECT * FROM sys.default_constraints WHERE name = N'PK_LangID') 
-  ALTER TABLE Languages DROP CONSTRAINT [PK_LangID];
-IF EXISTS (SELECT * FROM information_schema.tables WHERE table_name = N'Languages') 
-  DROP TABLE Languages;
-CREATE TABLE Languages (
-  Lang_ID   int IDENTITY(1,1),
-  Lang      varchar(50) NOT NULL,
-  CONSTRAINT [PK_LangID] PRIMARY KEY (Lang_ID ASC),
-  CONSTRAINT [CHK_Lang] CHECK (DATALENGTH(Lang) > 0)
-);
-
-INSERT INTO Languages (Lang) VALUES ('English');
-INSERT INTO Languages (Lang) VALUES ('French');
-INSERT INTO Languages (Lang) VALUES ('Italian');
-INSERT INTO Languages (Lang) VALUES ('Russian');
-INSERT INTO Languages (Lang) VALUES ('Vietnamese');
 
 /********************************************************************************
                             LATIN ALPHABET RELATION
@@ -79,16 +58,63 @@ INSERT INTO LatinAlphabet (Letter) VALUES ('Y');
 INSERT INTO LatinAlphabet (Letter) VALUES ('Z');
 
 /********************************************************************************
+                            LANGUAGES RELATION
+*********************************************************************************/
+IF EXISTS (SELECT * FROM sys.default_constraints WHERE name = N'PK_LangID') 
+  ALTER TABLE Languages DROP CONSTRAINT [PK_LangID];
+IF EXISTS (SELECT * FROM information_schema.tables WHERE table_name = N'Languages') 
+  DROP TABLE Languages;
+CREATE TABLE Languages (
+  Lang_ID   int IDENTITY(1,1),
+  Lang      varchar(50) NOT NULL,
+  CONSTRAINT [PK_LangID] PRIMARY KEY (Lang_ID ASC),
+  CONSTRAINT [CHK_Lang] CHECK (DATALENGTH(Lang) > 0)
+);
+
+INSERT INTO Languages (Lang) VALUES ('English');
+INSERT INTO Languages (Lang) VALUES ('French');
+INSERT INTO Languages (Lang) VALUES ('Italian');
+INSERT INTO Languages (Lang) VALUES ('Russian');
+INSERT INTO Languages (Lang) VALUES ('Vietnamese');
+
+/********************************************************************************
+                                ALPHABETS RELATION
+*********************************************************************************/
+IF EXISTS (SELECT * FROM sys.default_constraints WHERE name = N'PK_AlphabetID') 
+  ALTER TABLE Alphabets DROP CONSTRAINT [PK_AlphabetID];
+IF EXISTS (SELECT * FROM information_schema.tables WHERE table_name = N'Alphabets') 
+  DROP TABLE Alphabets;
+CREATE TABLE Alphabets (
+  Alphabet_ID         int IDENTITY(1,1),
+  AlphabetFamily      varchar(50) NOT NULL,
+  CONSTRAINT [PK_AlphabetID] PRIMARY KEY (Alphabet_ID ASC),
+  CONSTRAINT [CHK_AlpFam] CHECK (DATALENGTH(AlphabetFamily) > 0)
+);
+
+INSERT INTO Alphabets (AlphabetFamily) VALUES ('Latin Alphabet');
+INSERT INTO Alphabets (AlphabetFamily) VALUES ('Cyrillic Alphabet');
+
+/********************************************************************************
                             LANGUAGE ALPHABET RELATION
 *********************************************************************************/
 IF EXISTS (SELECT * FROM sys.default_constraints WHERE name = N'PK_LangAlphaID') 
   ALTER TABLE LanguageAlphabet DROP CONSTRAINT [PK_LangAlphaID];
+IF EXISTS (SELECT * FROM sys.default_constraints WHERE Name = N'FKLangAlphaLangID')
+  ALTER TABLE LanguageAlphabet DROP CONSTRAINT [FKLangAlphaLangID];
+IF EXISTS (SELECT * FROM sys.default_constraints WHERE Name = N'FKLangAlphaAlphaID')
+  ALTER TABLE LanguageAlphabet DROP CONSTRAINT [FKLangAlphaAlphaID];
 IF EXISTS (SELECT * FROM information_schema.tables WHERE table_name = N'LanguageAlphabet') 
   DROP TABLE LanguageAlphabet;
 CREATE TABLE LanguageAlphabet (
-  LangAlpha_ID   int IDENTITY(1,1),
-  Lang           int UNIQUE NOT NULL,
-  Alphabet       int NOT NULL,
+  LangAlpha_ID		int IDENTITY(1,1),
+  Lang_ID           int UNIQUE NOT NULL,
+  Alphabet_ID       int NOT NULL,
   CONSTRAINT [PK_LangAlphaID] PRIMARY KEY (LangAlpha_ID ASC),
-  /* TODO: Add a check constraint for joint relation */
+  CONSTRAINT [FKLangAlphaLangID] FOREIGN KEY (Lang_ID) REFERENCES Languages(Lang_ID) 
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT [FKLangAlphaAlphaID] FOREIGN KEY (Alphabet_ID) REFERENCES Alphabets(Alphabet_ID) 
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO LanguageAlphabet (Lang_ID, Alphabet_ID) VALUES (1, 1);
+INSERT INTO LanguageAlphabet (Lang_ID, Alphabet_ID) VALUES (4, 2);
