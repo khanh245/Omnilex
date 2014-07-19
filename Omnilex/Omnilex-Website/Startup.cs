636e56cd19d37b11c4ecf1fd6300fb75ef8980e5
Khanh Nguyen <knultimate@live.com>
2014-07-19 14:45:32 -0700

    Initial Omnilex Skeleton
    Added Azure website and registered an azure website

---
+++ Omnilex/Omnilex-Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Omnilex_Website.Startup))]
+namespace Omnilex_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
