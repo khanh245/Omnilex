d58bf65c51d29e190545dfe1f86f1c4c638f547c
Khanh Nguyen <knultimate@live.com>
2014-07-27 11:02:21 -0700

    Problems with WCF Config file

--- Omnilex/Omnilex-Service/OmnilexService.svc.cs
+++ Omnilex/Omnilex-Service/OmnilexService.svc.cs
@@ -7,8 +7,6 @@ using System.Text;
 
 namespace OmnilexService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "OmnilexService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select OmnilexService.svc or OmnilexService.svc.cs at the Solution Explorer and start debugging.
     public class OmnilexService : IOmnilexService
     {
         public string DoWork()
