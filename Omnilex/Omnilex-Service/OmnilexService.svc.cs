7afd0898c6607d577b36ed16de1a343e9666a277
Khanh Nguyen <knultimate@live.com>
2014-07-27 12:05:51 -0700

    Service is up and running
    Needs some security

--- Omnilex/Omnilex-Service/OmnilexService.svc.cs
+++ Omnilex/Omnilex-Service/OmnilexService.svc.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.Text;
 
 namespace OmnilexService
 {
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+
     public class OmnilexService : IOmnilexService
     {
         public string DoWork()
