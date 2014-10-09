61c9b3c38fb4d73937d6694f3694ef3fe4b1d8fd
Khanh Nguyen <knultimate@live.com>
2014-10-08 22:20:23 -0700

    Home Page
    Working Caret

---
+++ Omnilex/Omnilex-Website/Scripts/SiteScript.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $(".dropdown-menu li").click(function () {
+        var selText = $(this).text();
+        $(this).closest('.input-group-btn').children('button').html(selText + ' <span class="caret"></span>')
+    });
+});
