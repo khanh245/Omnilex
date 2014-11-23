0939c730c4ed8edd2189c699962f7bdb2859f2b6
Khanh Nguyen <knultimate@live.com>
2014-11-23 01:54:28 -0800

    The Purge
    Unfortunately...it has to be done..

--- Omnilex/Omnilex-Website/Scripts/SiteScript.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $(".dropdown-menu li").click(function () {
-        var selText = $(this).text();
-        $(this).closest('.input-group-btn').children('button').html(selText + ' <span class="caret"></span>')
-    });
-});
